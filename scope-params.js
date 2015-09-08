550356323d5773a083b1740012c9a6416e397553
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-09-08 16:54:58 +0200

    create scope params

---
+++ scope-params.js
@@ -0,0 +1,14 @@
+angular.module('ScopeParams', [])
+.controller('ScopeParams', ['$scope', function($scope) {
+    $scope._params = {};
+    $scope.setParam = function(name, value) {
+      $scope._params[name] = value;
+    };
+    $scope.getParam = function(name) {
+      return $scope._params[name];
+    };
+    $scope.watchParam = function(name, fn, recursive) {
+      $scope.$watch('_params["'+name+'"]', fn);
+    };
+  }]);
+
